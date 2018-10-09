607f429aaf9147d2eac4836965d57709072256c6
Jeff Musa <jeffmusa@gmail.com>
2018-10-09 18:54:02 +0300

    suicide

--- InstaBoom/settings.py
+++ InstaBoom/settings.py
@@ -113,10 +113,12 @@ else:
            default=config('DATABASE_URL')
        )
    }
+
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
